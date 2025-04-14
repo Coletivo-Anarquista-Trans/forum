1f02b703bd89558259efc8bb2fb6d96db4425462
Mazrine <mazrine@protonmail.com>
2025-04-14 13:16:20 -0300

    add flake.nix and icon path to dioxus.toml

--- flake.nix
+++ flake.nix
@@ -1,28 +1,42 @@
+# flake.nix
 {
-  description = "Dioxus Desktop App with GTK Dev Env";
+  description = "Nix flake for GTK and related dependencies";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }: {
-    devShells.default = let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in pkgs.mkShell {
-      buildInputs = [
-        pkgs.pkg-config
-        pkgs.gtk3
-        pkgs.glib
-        pkgs.gdk-pixbuf
-        pkgs.cairo
-        pkgs.pango
-        pkgs.at-spi2-core
-        pkgs.openssl
-        pkgs.libxkbcommon
-      ];
-
-      # Make sure pkg-config finds .pc files
-      PKG_CONFIG_PATH = pkgs.lib.makeLibraryPath buildInputs + "/lib/pkgconfig";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-}
 
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          pkgs.pkg-config
+          pkgs.gtk3
+          pkgs.gdk-pixbuf
+          pkgs.pango
+          pkgs.cairo
+          pkgs.glib
+          pkgs.openssl
+          pkgs.webkitgtk_4_1
+          pkgs.webkitgtk
+          pkgs.xdotool
+        ];
+
+        shellHook = ''
+          export PKG_CONFIG_PATH=${
+            pkgs.gtk3.dev}/lib/pkgconfig:${
+            pkgs.gdk-pixbuf.dev}/lib/pkgconfig:${
+            pkgs.pango.dev}/lib/pkgconfig:${
+            pkgs.cairo.dev}/lib/pkgconfig:${
+            pkgs.glib.dev}/lib/pkgconfig:${
+            pkgs.openssl.dev}/lib/pkgconfig:${
+            pkgs.webkitgtk.dev}/lib/pkgconfig:${
+            pkgs.xdotool}/lib/pkgconfig:${
+            pkgs.webkitgtk_4_1.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
+        '';
+      };
+    });
+}
